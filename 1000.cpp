986bab4ee47726fb3dca00e6e53c0b5c395b98c6
C0deTrash <geek0lychee@gmail.com>
2018-01-31 14:48:19 +0800

    Problem 1000 example submit

---
+++ 1000.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  int numberA = 0, numberB = 0;
+  cin >> numberA >> numberB;
+  cout << (numberA + numberB) << endl;
+
+  return 0;
+}
